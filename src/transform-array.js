b95a5726c8a7425feecf38aa343742344427e0c7
Heliken <burshf@gmail.com>
2020-10-07 12:28:04 +0300

    feat: implement transform array function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(array) {
+  if(Array.isArray(array)){
+    let newArray = [...array];
+
+    isStringCommand = (string) =>{
+      return string.toString().startsWith("--")
+    }
+
+    newArray.forEach((value, i) => {
+      if(typeof value === 'string' && value.startsWith("--")) {
+        let isArrayStart = (i === 0);
+        let isArrayEnd = (i === newArray.length - 1);
+        switch(value) {
+          case "--discard-prev":
+            if(!isArrayStart && !isStringCommand(newArray[i-1]))
+              newArray.splice(i-1,1)
+            break
+          case "--discard-next":
+            if(!isArrayEnd)
+              newArray.splice(i+1,1)
+            break
+          case "--double-prev":
+            if(!isArrayStart && !isStringCommand(newArray[i-1]))
+              newArray[i] = newArray[i-1]
+            break
+          case "--double-next":
+            if(!isArrayEnd && !isStringCommand(newArray[i+1]))
+              newArray[i] = newArray[i+1]
+            break
+          default:
+            break
+        }
+      }
+    })
+    
+    return newArray.filter(x => !x.toString().startsWith('--'))
+  }
+
+  throw "Error"
+}
