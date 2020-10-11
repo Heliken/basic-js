ef7033594db10f9fdf0a0f62eb64a0799759a2ac
Heliken <burshf@gmail.com>
2020-10-11 19:52:34 +0300

    feat: implement vigenere-cipher class

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,66 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(notReverse = true) {
+    this.notReverse = notReverse;
+    this.encryptTable = this.createEncryptTable();
+  }
+  createEncryptTable() {
+    let encryptTable = [];
+    for(let i = 0; i < 26; i++){
+      let encryptRow = [];
+      for(let j = 0; j < 26; j++){
+        encryptRow.push(String.fromCharCode(((j + i) % 26) + 65));
+      }
+      encryptTable.push(encryptRow)
+    }
+    return encryptTable
+  }
+  encrypt(phrase,keyword) {
+    if(phrase !== undefined && keyword !== undefined){
+      phrase = phrase.toUpperCase();
+      keyword = keyword.toUpperCase();
+      let encryptedPhrase = '';
+      let specialSymbolsCounter = 0;
+      for(let i = 0;i< phrase.length; i++){
+        let isSymbolLetter = /([A-Z])/.test(phrase[i]);
+        if(isSymbolLetter){
+          let colNumber = phrase[i].charCodeAt() - 65;
+          let rowNumber = keyword[(i -  specialSymbolsCounter) % keyword.length].charCodeAt() - 65;
+          let newSymbol = this.encryptTable[colNumber][rowNumber];
+          encryptedPhrase+=newSymbol;
+        } else {
+          specialSymbolsCounter++;
+          encryptedPhrase+=phrase[i];
+        }
+      }
+      return this.notReverse ? encryptedPhrase : encryptedPhrase.split("").reverse("").join("")
+    } else{
+      throw "Wrong values"
+    }
+  }   
+  decrypt(phrase, keyword) {
+    if(phrase !== undefined && keyword !== undefined){
+      phrase = phrase.toUpperCase();
+      keyword = keyword.toUpperCase();
+      let decryptedPhrase = '';
+      let specialSymbolsCounter = 0;
+      for(let i = 0;i< phrase.length; i++){
+        let isSymbolLetter = /([A-Z])/.test(phrase[i]);
+        if(isSymbolLetter){
+          let rowNumber = keyword[(i -  specialSymbolsCounter) % keyword.length].charCodeAt() - 65
+          let colNumber = this.encryptTable[rowNumber].indexOf(phrase[i]);
+          let newSymbol = this.encryptTable[0][colNumber];
+          decryptedPhrase+=newSymbol;
+        } else {
+          specialSymbolsCounter++;
+          decryptedPhrase+=phrase[i];
+        }
+      }
+      return this.notReverse ? decryptedPhrase : decryptedPhrase.split("").reverse("").join("")
+    } else{
+      throw "Wrong values"
+    }
   }
 }
 
